798e24166ea77866faba2f8a04890ca398bbb017
johneckert <johnteckert@gmail.com>
2018-04-03 11:46:09 -0400

    added browser warning for non chrome

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import AuthContainer from './containers/AuthContainer';
 import LoginContainer from './containers/LoginContainer';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import BrowserDetection from 'react-browser-detection';
 import './App.css';
+
+const BrowserHandler = {
+  chrome: () => <div />,
+  default: browser => (
+    <div className="browser-warning">
+      Duckie uses the JavaScript Web Speech API and requires Chrome to operate
+    </div>
+  )
+};
 
 class App extends Component {
   render() {
     return (
       <div className="background">
+        <BrowserDetection>{BrowserHandler}</BrowserDetection>
         <Header />
         <Switch>
           <Route path="/login" component={LoginContainer} />
