a67e12cb98f7a79bc4cc8bba441929b9f9535859
johneckert <johnteckert@gmail.com>
2018-05-01 11:50:30 -0400

    dashboard css for phone done

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -16,7 +16,7 @@ class DashboardContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="dashboard-outer">
         <TinyDuck />
         <div className="dashboard-container">
           <Greeting />
