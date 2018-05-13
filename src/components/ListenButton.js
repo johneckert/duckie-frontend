68e0b54f0633295aeadff6b7e5da40e8bceba9bc
John Eckert <johnteckert@gmail.com>
2018-05-13 16:36:40 -0400

    made listen button disable in unsupported browsers

--- src/components/ListenButton.js
+++ src/components/ListenButton.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import BrowserDetection from 'react-browser-detection';
 import { connect } from 'react-redux';
 import { toggleListening } from '../actions/actions';
 
 const ListenButton = props => {
-  return (
-    <button onClick={props.dispatchToggleListening} className="listen-button">
-      {props.listening ? 'STOP LISTENING' : 'START LISTENING'}
-    </button>
-  );
+  const BrowserHandler = {
+    chrome: () => (
+      <button onClick={props.dispatchToggleListening} className="listen-button">
+        {props.listening ? 'STOP LISTENING' : 'START LISTENING'}
+      </button>
+    ),
+    default: () => (
+      <button className="listen-button">
+        {props.listening ? 'STOP LISTENING' : 'START LISTENING'}
+      </button>
+    )
+  };
+
+  return <BrowserDetection>{BrowserHandler}</BrowserDetection>;
 };
 
 const mapStateToProps = state => {
