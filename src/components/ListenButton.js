4655f789491c65df99697500ed81b1960e4c3d59
John Eckert <johnteckert@gmail.com>
2018-05-13 16:56:45 -0400

    undid bad browser handling

--- src/components/ListenButton.js
+++ src/components/ListenButton.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import BrowserDetection from 'react-browser-detection';
 import { connect } from 'react-redux';
 import { toggleListening } from '../actions/actions';
 
 const ListenButton = props => {
-  const BrowserHandler = {
-    chrome: (props) => (
-      <button onClick={props.dispatchToggleListening} className="listen-button">
-        {props.listening ? 'STOP LISTENING' : 'START LISTENING'}
-      </button>
-    ),
-    default: (props) => (
-      <button className="listen-button">
-        {props.listening ? 'STOP LISTENING' : 'START LISTENING'}
-      </button>
-    )
-  };
-
-  return <BrowserDetection>{BrowserHandler}</BrowserDetection>;
+  return (
+    <button onClick={props.dispatchToggleListening} className="listen-button">
+      {props.listening ? 'STOP LISTENING' : 'START LISTENING'}
+    </button>
+  );
 };
 
 const mapStateToProps = state => {
